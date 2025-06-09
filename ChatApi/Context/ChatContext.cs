a1f1ce6bf1fd353df617e63ac8812ea6523f109d
Glebson Santos <glebsonsantos@outlook.pt>
2025-06-09 00:25:56 -0300

    feat: db context and User model

---
+++ ChatApi/Context/ChatContext.cs
@@ -0,0 +1,15 @@
+﻿using ChatApi.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatApi.Context
+{
+    public class ChatContext : IdentityDbContext<User>
+    {
+        public ChatContext(DbContextOptions<ChatContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ ChatApi/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ChatApi.Models
+{
+    public class User : IdentityUser
+    {
+        public string? RefreshToken { get; set; }
+        public DateTime? ValidRefreshToken { get; set; }
+    }
+}

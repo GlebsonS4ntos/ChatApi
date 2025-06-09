43a53c8ad8f7bac0aacaf92f2ddb0a83007b5ce4
Glebson Santos <glebsonsantos@outlook.pt>
2025-06-09 11:54:39 -0300

    feat: identity config and token config

--- ChatApi/Program.cs
+++ ChatApi/Program.cs
@@ -1,6 +1,8 @@
 
 using ChatApi.Context;
+using ChatApi.Helpers;
 using ChatApi.Models;
+using ChatApi.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -24,9 +26,21 @@ namespace ChatApi
                 opt.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection"));
             });
 
-            builder.Services.AddIdentity<User, IdentityRole>()
-                .AddEntityFrameworkStores<ChatContext>()
+            builder.Services.AddIdentity<User, IdentityRole>(opt =>
+            {
+                opt.User.RequireUniqueEmail = true;
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequireDigit = false;
+                opt.Password.RequireLowercase = false;
+                opt.Password.RequireUppercase = false;
+            }).AddEntityFrameworkStores<ChatContext>()
                 .AddDefaultTokenProviders();
+
+            builder.Services.Configure<JwtConfig>(
+               builder.Configuration.GetSection("JwtSettings")
+            );
+
+            builder.Services.AddScoped<JwtService>();
 
             var app = builder.Build();
 

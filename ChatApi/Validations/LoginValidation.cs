806b14e7174c9abd141a1b11a5e580b45ff4c0ab
Glebson Santos <glebsonsantos@outlook.pt>
2025-06-10 01:29:33 -0300

    feat: login validation

---
+++ ChatApi/Validations/LoginValidation.cs
@@ -0,0 +1,21 @@
+﻿using ChatApi.Dto;
+using FluentValidation;
+
+namespace ChatApi.Validations
+{
+    public class LoginValidation : AbstractValidator<LoginDto>
+    {
+        public LoginValidation() 
+        {
+            RuleFor(u => u.UserEmail)
+                .NotEmpty()
+                .EmailAddress().WithMessage("E-mail inválido.");
+
+            RuleFor(u => u.Password)
+                .NotEmpty()
+                .NotNull()
+                .MinimumLength(6).WithMessage("A senha deve ter no mínimo 6 caracteres.")
+                .MaximumLength(20).WithMessage("A senha não deve ter mais de 20 caracteres.");
+        }
+    }
+}

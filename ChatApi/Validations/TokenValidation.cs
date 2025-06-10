18eab66eccaaf3bd389fa840e31b6e492c8420bd
Glebson Santos <glebsonsantos@outlook.pt>
2025-06-10 18:45:22 -0300

    feat: token validation

---
+++ ChatApi/Validations/TokenValidation.cs
@@ -0,0 +1,19 @@
+﻿using ChatApi.Dto;
+using FluentValidation;
+
+namespace ChatApi.Validations
+{
+    public class TokenValidation : AbstractValidator<TokenDto>
+    {
+        public TokenValidation() 
+        {
+            RuleFor(t => t.Accesstoken)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(t => t.RefreshToken)
+                .NotEmpty()
+                .NotNull();
+        }
+    }
+}

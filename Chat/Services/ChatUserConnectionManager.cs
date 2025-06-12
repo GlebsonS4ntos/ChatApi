39aee211b6a6d8e78776ec3c8113c5a430c8946d
Glebson Santos <glebsonsantos@outlook.pt>
2025-06-12 02:10:57 -0300

    feat: Add and remove connection to user

---
+++ Chat/Services/ChatUserConnectionManager.cs
@@ -0,0 +1,17 @@
+﻿namespace ChatCore.Services
+{
+    public class ChatUserConnectionManager
+    {
+        private readonly Dictionary<string, string> _userConnections = new();
+
+        public void AddUserConnection(string username, string connectionId)
+        {
+            _userConnections[username] = connectionId;
+        }
+
+        public void RemoveUserConnection(string username)
+        {
+            _userConnections.Remove(username);
+        }
+    }
+}

7053c8395e8c01ff626f0b5b89353f0783e31656
Glebson Santos <glebsonsantos@outlook.pt>
2025-06-12 02:11:27 -0300

    feat: add and remove connection endpoints

---
+++ Chat/Hubs/ChatHub.cs
@@ -0,0 +1,39 @@
+﻿using ChatCore.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Chat.Hubs
+{
+    [Authorize]
+    public class ChatHub : Hub
+    {
+        private readonly ChatUserConnectionManager _connectionManager;
+
+        public ChatHub(ChatUserConnectionManager connectionManager)
+        {
+            _connectionManager = connectionManager;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var username = Context.User.Claims.FirstOrDefault(c => c.Type == "userName")?.Value;
+
+            if (!string.IsNullOrEmpty(username))
+            {
+                var connectionId = Context.ConnectionId.ToString();
+                _connectionManager.AddUserConnection(username, connectionId);
+            }
+
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            var username = Context.User.Claims.FirstOrDefault(c => c.Type == "userName")?.Value;
+
+            if (!string.IsNullOrEmpty(username)) _connectionManager.RemoveUserConnection(username);
+
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}

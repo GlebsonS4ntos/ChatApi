5a66038fd13558b36030fdd38a18559f48f21d6c
Glebson Santos <glebsonsantos@outlook.pt>
2025-06-12 02:07:28 -0300

    feat: chat service extension

---
+++ Chat/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using ChatCore.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Chat.Extensions
+{
+    public static class ServiceCollectionExtension
+    { 
+        public static IServiceCollection AddChat(this IServiceCollection service)
+        {
+            service.AddSignalRCore();
+            service.AddSingleton<ChatUserConnectionManager>();
+
+            return service;
+        }
+    }
+}

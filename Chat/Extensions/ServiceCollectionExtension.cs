03a225667628abb93c24c71b0a729b4497b1ac8a
Glebson Santos <glebsonsantos@outlook.pt>
2025-06-16 00:35:36 -0300

    fix: signalR injection dependency and version to Authorization

--- Chat/Extensions/ServiceCollectionExtension.cs
+++ Chat/Extensions/ServiceCollectionExtension.cs
@@ -1,13 +1,12 @@
-﻿using ChatCore.Services;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 
 namespace Chat.Extensions
 {
     public static class ServiceCollectionExtension
-    { 
+    {
         public static IServiceCollection AddChat(this IServiceCollection service)
         {
-            service.AddSignalRCore();
+            service.AddSignalR(); 
             service.AddSingleton<ChatUserConnectionManager>();
 
             return service;
